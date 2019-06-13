0b2e79f4adda978ef465731f447625edc90de896
DiamondYuan <admin@diamondyuan.com>
2019-06-14 00:01:12 +0800

    refactor: use hooks

--- packages/vanaheim-website/src/pages/comic/recent.tsx
+++ packages/vanaheim-website/src/pages/comic/recent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'dva';
 import { GlobalState, UmiComponentProps } from '@/common/types';
 import { asyncGetComic, setList } from '@/actions/comic';
@@ -6,44 +6,39 @@ import { Col, Row } from 'antd';
 import styles from './recent.scss';
 import ContextMenu from './components/ContextMenu';
 
-const mapStateToProps = ({ comic, loading }: GlobalState) => ({ comic, loading });
+const mapStateToProps = ({ comic: { list } }: GlobalState) => ({ list });
 type PageStateProps = ReturnType<typeof mapStateToProps>;
 type PageProps = PageStateProps & UmiComponentProps;
-type PageState = {};
 
-class RecentComic extends React.PureComponent<PageProps, PageState> {
-  componentDidMount() {
-    this.props.dispatch(
+const RecentComic: React.FC<PageProps> = ({ list, dispatch }: PageProps) => {
+  useEffect(() => {
+    dispatch(
       asyncGetComic({
         offset: 0,
       })
     );
-  }
+    return function clean() {
+      dispatch(setList([]));
+    };
+  }, []);
 
-  componentWillUnmount = () => {
-    this.props.dispatch(setList([]));
-  };
+  return (
+    <div style={{ background: 'white', padding: 24 }}>
+      <Row gutter={20} type="flex">
+        {list.map(o => (
+          <Col key={o.id} xs={12} md={8} lg={6} xl={4}>
+            <ContextMenu comicId={o.id}>
+              <div
+                className={styles.cover}
+                style={{ backgroundImage: `url(/server-static/cover/${o.id})` }}
+              />
+            </ContextMenu>
+            <p>{o.title}</p>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
 
-  render() {
-    const { list } = this.props.comic;
-    return (
-      <div style={{ background: 'white', padding: 24 }}>
-        <Row gutter={20} type="flex">
-          {list.map(o => (
-            <Col key={o.id} xs={12} md={8} lg={6} xl={4}>
-              <ContextMenu comicId={o.id}>
-                <div
-                  className={styles.cover}
-                  style={{ backgroundImage: `url(/server-static/cover/${o.id})` }}
-                />
-              </ContextMenu>
-              <p>{o.title}</p>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    );
-  }
-}
-
-export default connect(mapStateToProps)(RecentComic);
+export default connect(mapStateToProps)(React.memo(RecentComic));
